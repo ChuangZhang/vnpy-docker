a868dd79dc090d1d174e7f9152a44c7079c08059
zchuang <zhangchuang185@gmail.com>
2022-08-11 16:49:51 +0800

    vnpy runnit script optimize

--- runnit.sh
+++ runnit.sh
@@ -1,6 +1,7 @@
 dmidecode_info=$(whereis -b dmidecode)
-dmidecode_path=${dmidecode_info#*:}
-sudo docker run --name data--privileged \
+dmidecode_path=`echo ${dmidecode_info#*:}`
+echo $dmidecode_path
+sudo docker run --name data --privileged \
 -v ${dmidecode_path}:${dmidecode_path} -v /opt/data/vnpy:/var/vnpy -v /dev/mem:/dev/mem \
 -p 222:222 -p 18124:18124 -p 18123:18123 -t -d -i zchuang:vnpy \
 /bin/bash /root/.profile
